0e1f4c36c138f0a229f6239baff72e451e31ed7a
fira <loyauflorian@gmail.com>
2021-10-20 23:21:45 +0000

    mix branches liveview fixes

--- lib/tpanel_web/live/branch_live_view.ex
+++ lib/tpanel_web/live/branch_live_view.ex
@@ -3,13 +3,16 @@ defmodule TpanelWeb.BranchLiveView do
 
   def mount(_params, %{"test_mix_id" => test_mix_id}, socket) do
     test_mix = Tpanel.GitTools.get_full_test_mix!(test_mix_id)
-    {:ok, assign(socket, test_mix: test_mix, newchangeset: Tpanel.GitTools.change_branch(%Tpanel.GitTools.Branch{}))}
+    topic = "mixbranches_#{test_mix.id}"
+    TpanelWeb.Endpoint.subscribe(topic)
+    changeset = Tpanel.GitTools.change_branch(%Tpanel.GitTools.Branch{})
+    {:ok, assign(socket, test_mix: test_mix, changeset: changeset, topic: topic)}
   end
 
   def handle_event("delete_branch", %{"branch" => branch_id}, socket) do
     Tpanel.GitTools.get_branch!(branch_id)
-      |> Tpanel.GitTools.delete_branch
-    put_flash(socket, :info, "Branch deleted")
+    |> Tpanel.GitTools.delete_branch
+    TpanelWeb.Endpoint.broadcast(socket.assigns.topic, "deleted", %{})
     {:noreply, socket}
   end
 
@@ -18,13 +21,19 @@ defmodule TpanelWeb.BranchLiveView do
     |> Tpanel.GitTools.create_mix_branch(branch_changeset)
     |> case do
       {:ok, branch} ->
+        TpanelWeb.Endpoint.broadcast(socket.assigns.topic, "created", %{})
         {:noreply, 
         socket 
-        |> put_flash(:info, "Branch '#{branch.name}' created")
-        |> assign(newchangeset: Tpanel.GitTools.change_branch(%Tpanel.GitTools.Branch{}))
+        |> assign(changeset: Tpanel.GitTools.change_branch(%Tpanel.GitTools.Branch{}))
         }
-      {:error, %Ecto.Changeset{} = newchangeset} ->
-        {:noreply, assign(socket, newchangeset: newchangeset)}
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
     end
+  end
+
+  def handle_info(_event, socket) do
+    test_mix = Tpanel.GitTools.get_full_test_mix!(socket.assigns.test_mix.id)
+    changeset = Tpanel.GitTools.change_branch(%Tpanel.GitTools.Branch{})
+    {:noreply, assign(socket, test_mix: test_mix, changeset: changeset)}
   end
 end
