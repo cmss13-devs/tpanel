1eafcfd0f9a680881103bfbe023b7d5737293b24
fira <loyauflorian@gmail.com>
2021-10-20 14:48:02 +0000

    form tweaks

--- lib/tpanel_web/controllers/page_controller.ex
+++ lib/tpanel_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule TpanelWeb.PageController do
   use TpanelWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    new_path = Routes.test_mix_path(conn, :index)
+    redirect(conn, to: new_path)
+    |> halt()
   end
 end
