ee4cdc43a0c3e8872c7d73fe6402d9530f5748c5
fira <loyauflorian@gmail.com>
2022-04-18 00:56:26 +0000

    Fix invalid characters being allowed in some branch params

--- lib/tpanel/git_tools/branch.ex
+++ lib/tpanel/git_tools/branch.ex
@@ -18,9 +18,9 @@ defmodule Tpanel.GitTools.Branch do
     branch
     |> cast(attrs, [:name, :remote, :refspec, :target_revision, :fetched_revision, :built_revision, :priority])
     |> validate_required([:name, :remote, :refspec])
-    |> validate_format(:name, ~r/^[0-9a-zA-Z\-_]+/)
-    |> validate_format(:remote, ~r/^[0-9a-zA-Z\-_\/:%@]+/)
-    |> validate_format(:refspec, ~r/^[0-9a-zA-Z\-_\/]+/)
+    |> validate_format(:name, ~r/^[0-9a-zA-Z\.\-_]+$/)
+    |> validate_format(:remote, ~r/^[0-9a-zA-Z\.\-_\/:%@]+$/)
+    |> validate_format(:refspec, ~r/^[0-9a-zA-Z\.\-_\/]+$/)
     |> validate_format(:target_revision, ~r/^[[:alnum:]]{40}|$/)
     |> validate_format(:fetched_revision, ~r/^[[:alnum:]]{40}$/)
     |> validate_format(:built_revision, ~r/^[[:alnum:]]{40}$/)
